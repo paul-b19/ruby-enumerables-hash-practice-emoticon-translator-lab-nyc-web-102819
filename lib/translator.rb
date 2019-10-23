342a679d395dc7030694d5505d44b6f1b7598b4a
Pavel Beletsky <pavel.beletsky@live.com>
2019-10-23 17:14:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,11 +3,15 @@ require 'pp'
 
 def load_library(path)
   library = {}
-  new_library = {}
+  new_library = {get_meaning: {}, get_emoticon: {}}
   library = YAML.load_file(path)
-  library.map do |key, value|
-    
   pp library
+  library.map do |key, value|
+    new_library[get_meaning][value] = key;
+    new_library[get_emoticon][key] = value;
+  end
+    
+  pp new_library
 end
 
 def get_japanese_emoticon
